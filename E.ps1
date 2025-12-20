c6ccd5d5f46aa5560a6d5beb669a72e081703c1c
dontfuckmybrain <ilia.derum@gmail.com>
2025-12-20 13:50:43 +0300

    Update E.ps1

--- E.ps1
+++ E.ps1
@@ -35,252 +35,82 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit 1
 }
 
-Write-Host "Java args:"
-Get-CimInstance Win32_Process | Where-Object {$_.Name -like '*java*'} | ForEach-Object { $splitText = ((string -split '-Djava.library.path=')[1] -split '"')[0]; Write-Host $splitText }
-Write-Host ""
-
-#Check deleted files
-Invoke-WebRequest 'https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/2.exe' -OutFile '3.exe'
-
-$driveLetters = @(Get-Volume | Where-Object {$_.DriveLetter} | ForEach-Object {$_.DriveLetter})
-$commandLine = @()
-
-foreach($i in $driveLetters) {
-    $commandLine += ./3.exe read $i -f '*.exe'
-}
-
-$needNext = 0
-$lastTime = ''
-
-if($commandLine) {
-    foreach($i in $commandLine -split '\n') {
-        if($needNext -eq 1) {
-            Write-Host 'Deleted .exe in ' $lastTime ' : ' ($i -split ' : ')[1].trim() -ForegroundColor Yellow
-            $needNext = 0
-        }
-
-        if($i.Contains('FILE_DELETE')) { 
-            $needNext = 1
-        }
-        if($i.Contains('Timestamp')) {
-            $lastTime = ($i -split ' : ')[1].trim()
-        }
-    }
-}
-
-
-$commandLine = @()
-
-foreach($i in $driveLetters) {
-    $commandLine += ./3.exe read $i -f '*.jar'
-}
-
-$needNext = 0
-$lastTime = ''
-
-if($commandLine) {
-    foreach($i in $commandLine -split '\n') {
-        if($needNext -eq 1) {
-            Write-Host 'Deleted .jar in ' $lastTime ' : ' ($i -split ' : ')[1].trim() -ForegroundColor Red
-            $needNext = 0
-        }
-
-        if($i.Contains('FILE_DELETE')) { 
-            $needNext = 1
-        }
-        if($i.Contains('Timestamp')) {
-            $lastTime = ($i -split ' : ')[1].trim()
-        }
-    }
-}
-
-
-$commandLine = @()
-
-foreach($i in $driveLetters) {
-    $commandLine += ./3.exe read $i -f '*funtime*' --dir-only
-}
-
-$needNext = 0
-$lastTime = ''
-
-if($commandLine) {
-    foreach($i in $commandLine -split '\n') {
-        if($needNext -eq 1) {
-            Write-Host 'Deleted baritone in ' $lastTime ' : ' ($i -split ' : ')[1].trim() -ForegroundColor Magenta
-            $needNext = 0
-        }
-
-        if($i.Contains('FILE_DELETE')) { 
-            $needNext = 1
-        }
-        if($i.Contains('Timestamp')) {
-            $lastTime = ($i -split ' : ')[1].trim()
-        }
-    }
-}
-if (Test-Path '3.exe') { Remove-Item -Path '3.exe' -Force }
-#END
-
-
-Start-Process powershell -ArgumentList "-Command", "Invoke-Expression (Invoke-RestMethod 'https://github.com/Ryodzaki/scripts/raw/refs/heads/main/services.ps1')"
-
 #$obsProcess = Get-Process -Name "obs64", "obs32", "obs", "ayugram", "telegram", "nvcontainer", "gamebar", "steam", "discord", "lively", "chrome", "opera", "msedge"
 #if ($obsProcess) { $obsProcess | Stop-Process -Force }
+
 
 $data = Get-Date -Format "dd.MM.yyyy HH:mm"
 
 
+Start-Process powershell -ArgumentList "-Command", "Invoke-Expression (Invoke-RestMethod 'https://github.com/Ryodzaki/scripts/raw/refs/heads/main/services.ps1')"
+Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/doomsday finder.exe" -OutFile "6.exe"
+Start-Process powershell -ArgumentList '-NoExit', '-Command', './6.exe; Remove-Item -Path "6.exe" -Force'
+
+
 #InjGen
-Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/1.exe" -OutFile "1.exe"
+Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/jmd.exe" -OutFile "1.exe"
 Invoke-Expression ".\1.exe"
 if (Test-Path "1.exe") { Remove-Item -Path "1.exe" -Force }
+Write-Host ""
 #END
+
+#DoomsdayCheck
+Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/dd check.exe" -OutFile "1.exe"
+Invoke-Expression ".\1.exe"
+if (Test-Path "1.exe") { Remove-Item -Path "1.exe" -Force }
+Write-Host ""
+#END
+
+Write-Host "Java args:"
+Get-CimInstance Win32_Process | Where-Object {$_.Name -like '*java*'} | ForEach-Object { $splitText = (([string]($_.CommandLine) -split '-Djava.library.path=')[1] -split '"')[0]; Write-Host $splitText }
+Write-Host ""
+
 
 #Connection info
 $connections = @(netstat -an | Where-Object { $_ -match "TCP.*2556.*ESTABLISHED" } | ForEach-Object { ($_ -split '\s+')[3] | Select-Object -First 1 })
-Write-Host "Connections: `n" $connections -join "`n" -ForegroundColor Yellow
+Write-Host "Connections: `n" ($connections -join "`n") -ForegroundColor Yellow
+Write-Host ""
 #END
 
 
 #Get DNS Data
 $dnsData = @(ipconfig /all | Select-String "DNS" | ForEach-Object { if ($_.ToString() -match "^([^:]+?)\s*:\s*(.*)$" -and $matches[2].Trim()) { $matches[2].Trim() } } | Where-Object { $_ })
-Write-Host "DNS Data: `n" $dnsData -join "`n" -ForegroundColor Yellow
+Write-Host "DNS Data: `n" ($dnsData -join "`n") -ForegroundColor Yellow
+Write-Host ""
 #END
-
 
 
 #Check USN
 $usn_deletes = @(try { Get-WinEvent -FilterXml "<QueryList><Query Id='0' Path='Microsoft-Windows-Ntfs/Operational'><Select Path='Microsoft-Windows-Ntfs/Operational'>*[System[EventID=501]] and *[EventData[Data[@Name='ProcessName'] and (Data='fsutil.exe')]]</Select></Query></QueryList>" -ErrorAction Stop | ForEach-Object { $_.TimeCreated.ToString("yyyy-MM-dd HH:mm:ss") } } catch { @() })
-Write-Host "USN Deleted: `n" $dnsData -join "`n" -ForegroundColor Yellow
-#END
-
+if($usn_deletes) {
+    Write-Host "USN Deleted: `n" ($usn_deletes -join "`n") -ForegroundColor Yellow
+}
+Write-Host ""
 
 #Check .dlls
 $javawDlls = @(Get-Process javaw -ErrorAction SilentlyContinue | ForEach-Object { $_.Modules } | Where-Object { $_.ModuleName -like "*.dll" -and -not $_.FileVersionInfo.FileDescription -and $_.FileName -notmatch "\\(natives|Temp)\\" } | Where-Object { (Get-AuthenticodeSignature $_.FileName).Status -ne 'Valid' } | Select-Object -ExpandProperty FileName)
 if ($javawDlls) { 
-    Write-Host "Suspicious dlls detected: `n" $javawDlls -join "`n" -ForegroundColor Yellow
+    Write-Host "Suspicious dlls detected: `n" ($javawDlls -join "`n") -ForegroundColor Yellow
 }
-
-
-
-
-Write-Host "Stage 3"
-
-
-
-#Search baritoen folders
-$baritoneDirs = @()
-
-Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/2.exe" -OutFile "1.exe"
-
-$driveLetters = @(Get-Volume | Where-Object {$_.DriveLetter} | ForEach-Object {$_.DriveLetter})
-$commandLine = @()
-
-foreach($i in $driveLetters) {
-    $commandLine += ./1.exe search $i -f cache --dir-only 
-}
-
-if($commandLine) {
-    foreach($i in $commandLine -split "\n") {
-        if($i.Contains("Path") -and $i.Contains("funtime")) { 
-            $baritoneDirs += ($i -split " : ")[1].trim()
-        }
-    }
-}
-
-    #Zone.ID
-$dooms = @()
-$zoneidExes = @()
-
-
-$commandLine = @()
-
-
-foreach($i in $driveLetters) {
-    $commandLine += ./1.exe search $i -f "*.exe" --file-only
-}
-
-if($commandLine) {
-    foreach($i in $commandLine -split "\n") {
-        if($i.Contains("Path")) { 
-            $cleanPath = ($i -split " : ")[1].trim()
-            if(Test-Path -LiteralPath $cleanPath) {
-                $content = (Get-Content -LiteralPath $cleanPath -Stream Zone.Identifier -ErrorAction SilentlyContinue) -split "`n"
-              
-                if($content) { 
-                    foreach($i in $content) {
-                        if($i.Contains("HostUrl=")) {
-                            $res = $cleanPath + "`n" + $i.Replace("HostUrl=", "").Trim()
-                            $zoneidExes += $res
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-$commandLine = @()
-Write-Host "Stage 3.1"
-
-foreach($i in $driveLetters) {
-    $commandLine += ./1.exe search $i -f "*.jar" --file-only
-}
-
-if($commandLine) {
-    foreach($i in $commandLine -split "\n") {
-        if($i.Contains("Path")) { 
-            $cleanPath = ($i -split " : ")[1].trim()
-            if(Test-Path -LiteralPath $cleanPath) {
-                $content = (Get-Content -LiteralPath $cleanPath -Stream Zone.Identifier -ErrorAction SilentlyContinue) -split "`n"
-              
-                if($content) { 
-                    foreach($i in $content) {
-                        if($i.Contains("HostUrl=") -and $i.Contains("doomsdayclient.com")) {
-                            $res = $cleanPath + "`n" + $i.Replace("HostUrl=", "").Trim()
-                            $dooms += $res
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-if (Test-Path "1.exe") { Remove-Item -Path "1.exe" -Force }
-
-if($baritoneDirs) { 
-    Write-Host "Baritone Dirs Detected: " ($baritoneDirs -join "`n") -ForegroundColor Yellow 
-}
-if($zoneidExes) { 
-    Write-Host "Suspicious exes: " ($zoneidExes -join "`n") -ForegroundColor Yellow 
-}
-if($dooms) { 
-    Write-Host "Doomsday Clients: " ($dooms -join "`n") -ForegroundColor Yellow 
-}
-#END
-
-
-
-
-
-
-
-Write-Host "Stage 4"
-
+Write-Host ""
 
 #VM Check
-Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/3.exe" -OutFile "1.exe"
-Invoke-Expression ".\1.exe"
+Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/vmcheck.exe" -OutFile "1.exe"
+Write-Host "VM check:"
+$puk = ./1.exe
+foreach($line in ($puk -split "`n")) {
+    if($line.Contains("CONCLUSION")) {
+        Write-Host $line
+    }
+}
 if (Test-Path "1.exe") { Remove-Item -Path "1.exe" -Force }
+Write-Host ""
 #END
-
-
 
 
 #BAM
 $bamData = @()
+Write-Host "Bam data:"
 function Get-Signature {
     param ([string[]]$FilePath)
 
@@ -355,6 +185,123 @@ if ($FilteredBam) {
         }
     }
 }
+Write-Host ""
+#END
+
+#Search baritone folders
+$baritoneDirs = @()
+
+Invoke-WebRequest "https://github.com/denisshiferfpidr/legit-check/raw/refs/heads/main/ntfspasrser.exe" -OutFile "1.exe"
+Write-Host "Check for baritone folders:"
+
+$driveLetters = @(Get-Volume | Where-Object {$_.DriveLetter} | ForEach-Object {$_.DriveLetter})
+$commandLine = @()
+
+foreach($i in $driveLetters) {
+    $commandLine += ./1.exe search $i -f cache --dir-only 
+}
+
+if($commandLine) {
+    foreach($i in $commandLine -split "\n") {
+        if($i.Contains("Path") -and $i.Contains("funtime")) { 
+            $baritoneDirs += ($i -split " : ")[1].trim()
+        }
+    }
+}
+
+    
+if($baritoneDirs) { 
+    Write-Host ($baritoneDirs -join "`n") -ForegroundColor Yellow 
+}
+Write-Host ""
+#END
+
+
+
+#Check deleted files
+Write-Host "Search for deleted files:"
+
+$driveLetters = @(Get-Volume | Where-Object {$_.DriveLetter} | ForEach-Object {$_.DriveLetter})
+
+$commandLine = @()
+
+foreach($i in $driveLetters) {
+    $commandLine += ./1.exe read $i -f '*funtime*' --dir-only
+}
+
+$needNext = 0
+$lastTime = ''
+
+if($commandLine) {
+    foreach($i in $commandLine -split '\n') {
+        if($needNext -eq 1) {
+            Write-Host 'Deleted baritone in ' $lastTime ' : ' ($i -split ' : ')[1].trim() -ForegroundColor Magenta
+            $needNext = 0
+        }
+
+        if($i.Contains('FILE_DELETE')) { 
+            $needNext = 1
+        }
+        if($i.Contains('Timestamp')) {
+            $lastTime = ($i -split ' : ')[1].trim()
+        }
+    }
+}
+
+
+$commandLine = @()
+
+foreach($i in $driveLetters) {
+    $commandLine += ./1.exe read $i -f '*.exe'
+}
+
+$needNext = 0
+$lastTime = ''
+
+if($commandLine) {
+    foreach($i in $commandLine -split '\n') {
+        if($needNext -eq 1) {
+            Write-Host 'Deleted .exe in ' $lastTime ' : ' ($i -split ' : ')[1].trim() -ForegroundColor Yellow
+            $needNext = 0
+        }
+
+        if($i.Contains('FILE_DELETE')) { 
+            $needNext = 1
+        }
+        if($i.Contains('Timestamp')) {
+            $lastTime = ($i -split ' : ')[1].trim()
+        }
+    }
+}
+
+
+$commandLine = @()
+
+foreach($i in $driveLetters) {
+    $commandLine += ./1.exe read $i -f '*.jar'
+}
+
+$needNext = 0
+$lastTime = ''
+
+if($commandLine) {
+    foreach($i in $commandLine -split '\n') {
+        if($needNext -eq 1) {
+            Write-Host 'Deleted .jar in ' $lastTime ' : ' ($i -split ' : ')[1].trim() -ForegroundColor Red
+            $needNext = 0
+        }
+
+        if($i.Contains('FILE_DELETE')) { 
+            $needNext = 1
+        }
+        if($i.Contains('Timestamp')) {
+            $lastTime = ($i -split ' : ')[1].trim()
+        }
+    }
+}
+
+if (Test-Path '1.exe') { Remove-Item -Path '1.exe' -Force }
+Write-Host ""
 #END
 
 
